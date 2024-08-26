5bdb2a81fafd8ad01dea75a9760f30dcfff3601b
Wesley <capin_g@hotmail.com>
2024-08-25 21:16:32 -0400

    dhdtyfydrh

---
+++ GameView/src/module-info.java
@@ -0,0 +1,14 @@
+module GameView {
+	exports Drawing;
+	exports FlatLandStructure;
+	exports View;
+
+	requires AntAnimation;
+	requires Monopoly.Island.Index;
+	requires ScreenIntegration;
+	requires SonificationStructure;
+	requires java.desktop;
+	requires trainingGround;
+
+	
+}
